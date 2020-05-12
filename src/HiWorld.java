a4311ea49fb76d57580548d222b2181ea23e6122
wangjian330 <1041405495@qq.com>
2020-05-12 11:00:52 +0800

    test

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -2,6 +2,7 @@ public class HiWorld {
     public static void main(String[] args) {
         System.out.println("hi world");
         System.out.println("hi world");
+        System.out.println("hi world");
 
         System.out.println("github is laji ");
     }
